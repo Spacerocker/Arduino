bf37987620ca5007a125747679b96d12e600bace
Spacerocker <72597312+Spacerocker@users.noreply.github.com>
2020-10-09 12:58:42 +0530

    Update lcd_display.ino
    Lcd

--- lcd_display/lcd_display.ino
+++ lcd_display/lcd_display.ino
@@ -1,7 +1,9 @@
-//YWROBOT
+//this is Lcd project
+
 //Compatible with the Arduino IDE 1.0
-//Library version:1.1
+//Library version:.1
 #include <LiquidCrystal_I2C.h>
+
 
 LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
