cb9d0014b14645fc40ddcba1eccb66dff52c22ba
Spacerocker <72597312+Spacerocker@users.noreply.github.com>
2020-10-24 21:42:58 +0530

    Update AutomatedWaterPlanting.ino
    correction

--- AutomatedWaterPlanting/AutomatedWaterPlanting.ino
+++ AutomatedWaterPlanting/AutomatedWaterPlanting.ino
@@ -1,23 +1,24 @@
 
-int Relay = 13; 
-int sensor = 7; 
+int RELAY_PIN = 13;  // Connect Pin 13 from arduino to Relay signal pin (IN)
+int SENSOR_PIN = 8;   // Connect the SENSOR Digitalout (D0) to Arduino 
 int val; 
 void setup() {
-  pinMode(13,OUTPUT); //Set pin 13 as OUTPUT pin, to send signal to relay
-  pinMode(8,INPUT); //Set pin 8 as input pin, to receive data from Soil moisture sensor.
+  pinMode(RELAY_PIN,OUTPUT); //Set pin 13 as OUTPUT pin, to send signal to relay
+  pinMode(SENSOR_PIN,INPUT); //Set pin 8 as input pin, to receive data from Soil moisture sensor.
   Serial.begin(9600);
 }
 
 void loop() { 
-  val = digitalRead(8); 
+  val = digitalRead(SENSOR_PIN); 
   Serial.println(val);
   if(val == LOW) 
   {
-  digitalWrite(13,HIGH); //if soil moisture sensor provides LOW value send LOW value to relay
+  digitalWrite(RELAY_PIN,HIGH); //if soil moisture sensor provides LOW value send LOW value to relay
   }
   else
   {
-  digitalWrite(13,LOW); //if soil moisture sensor provides HIGH value send HIGH value to relay
+  digitalWrite(RELAY_PIN,LOW); //if soil moisture sensor provides HIGH value send HIGH value to relay
   }
-  delay(400);
+  // wait for half second
+  delay(500);
 }
